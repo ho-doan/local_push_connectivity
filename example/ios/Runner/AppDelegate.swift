575c39c0d093331c3805eb43b56629fec4f3fcde
Ho Doan <doanho.it.dev@gmail.com>
2025-03-12 11:44:07 +0700

    update-ex-ios

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,13 +1,32 @@
 import Flutter
 import UIKit
 
+import local_push_connectivity
+
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+      GeneratedPluginRegistrant.register(with: self)
+      if let _ = Bundle.main.object(forInfoDictionaryKey: "NEAppPushBundleId") as? String{
+          PushConfigurationManager.shared.initialize()
+      }
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    override func applicationDidBecomeActive(_ application: UIApplication) {
+      let settingManager = SettingManager(nil)
+      var settings = settingManager.fetch()!
+      settings.appKilled = false
+      try? settingManager.set(settings: settings)
+    }
+    
+    override func applicationDidEnterBackground(_ application: UIApplication) {
+      let settingManager = SettingManager(nil)
+      var settings = settingManager.fetch()!
+      settings.appKilled = true
+      try? settingManager.set(settings: settings)
+    }
 }
